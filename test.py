a5bde9bf42193befb299700c03d395bc0ace0819
madurasenadeera <madura.sena@outlook.com>
2019-10-21 15:05:14 +0800

    Autocomplete functionality implemented

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
 import csv  # allows importing/manipulation/writing of csv files
-import tkinter as tk  # GUI
+from tkinter import *  # as tk  # GUI
+
+suburbList = []
+streetList = []
+with open('/Users/madurasenadeera/github/BCC_waste_collection_days/data/Waste Collection Days.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    for values in csv_reader:
+        suburbList.append(values[8])  # compiles column of suburbs into one list
+        streetList.append(values[5])
+
+    suburbs = list(dict.fromkeys(suburbList))  # removes duplicate suburbs
+    # print(suburbs)  # print list of suburbs
+    streets = list(dict.fromkeys(streetList))
+
+lista = suburbs  # IMPORTING SUBURBS
+listb = streets
 
 
 def searchProperty():
@@ -36,34 +52,117 @@ def searchProperty():
 ###########################
 
 
+class AutocompleteEntry(Entry):
+    def __init__(self, lista, *args, **kwargs):
+
+        Entry.__init__(self, *args, **kwargs)
+        self.lista = lista
+        self.var = self["textvariable"]
+        if self.var == '':
+            self.var = self["textvariable"] = StringVar()
+
+        self.var.trace('w', self.changed)
+        self.bind("<Right>", self.selection)
+        self.bind("<Up>", self.up)
+        self.bind("<Down>", self.down)
+
+        self.lb_up = False
+
+    def changed(self, name, index, mode):
+
+        if self.var.get() == '':
+            self.lb.destroy()
+            self.lb_up = False
+        else:
+            words = self.comparison()
+            if words:
+                if not self.lb_up:
+                    self.lb = Listbox()
+                    self.lb.bind("<Double-Button-1>", self.selection)
+                    self.lb.bind("<Right>", self.selection)
+                    self.lb.place(x=self.winfo_x(), y=self.winfo_y()+self.winfo_height())
+                    self.lb_up = True
+
+                self.lb.delete(0, END)
+                for w in words:
+                    self.lb.insert(END, w)
+            else:
+                if self.lb_up:
+                    self.lb.destroy()
+                    self.lb_up = False
+
+    def selection(self, event):
+
+        if self.lb_up:
+            self.var.set(self.lb.get(ACTIVE))
+            self.lb.destroy()
+            self.lb_up = False
+            self.icursor(END)
+
+    def up(self, event):
+
+        if self.lb_up:
+            if self.lb.curselection() == ():
+                index = '0'
+            else:
+                index = self.lb.curselection()[0]
+            if index != '0':
+                self.lb.selection_clear(first=index)
+                index = str(int(index)-1)
+                self.lb.selection_set(first=index)
+                self.lb.activate(index)
+
+    def down(self, event):
+
+        if self.lb_up:
+            if self.lb.curselection() == ():
+                index = '0'
+            else:
+                index = self.lb.curselection()[0]
+            if index != END:
+                self.lb.selection_clear(first=index)
+                index = str(int(index)+1)
+                self.lb.selection_set(first=index)
+                self.lb.activate(index)
+
+    def comparison(self):
+
+        test = self.var.get()  # retreives user inputted information from textbox
+        pattern = re.compile('.*' + test.upper() + '.*')  # comparing upper case value
+        return [w for w in self.lista if re.match(pattern, w)]
+
+
 ###########################
 # Initialising GUI window
 ###########################
 
-
-window = tk.Tk()
+window = Tk()
 window.title("BCC Waste Collection")
 # window.geometry("800x600")
 
 
-label = tk.Label(window, text="Waste Collection Day").pack()
+label = Label(window, text="Waste Collection Day").pack()
 
-suburbLabel = tk.Label(window, text="Suburb").pack()
-suburbEntry = tk.Entry(window)
+suburbLabel = Label(window, text="Suburb").pack()
+#suburbEntry = tk.Entry(window)
+
+suburbEntry = AutocompleteEntry(lista, window)
 suburbEntry.pack()
-streetLabel = tk.Label(window, text="Street").pack()
-streetEntry = tk.Entry(window)
+streetLabel = Label(window, text="Street").pack()
+#streetEntry = Entry(window)
+# streetEntry.pack()
+streetEntry = AutocompleteEntry(listb, window)
 streetEntry.pack()
-numberLabel = tk.Label(window, text="House number").pack()
-numberEntry = tk.Entry(window)
+numberLabel = Label(window, text="House number").pack()
+numberEntry = Entry(window)
 numberEntry.pack()
 
-searchButton = tk.Button(window, text="Search", command=searchProperty).pack()
+searchButton = Button(window, text="Search", command=searchProperty).pack()
 
-searchLabel = tk.Label(window)
+searchLabel = Label(window)
 searchLabel.pack()
 
-frame = tk.Frame(window)
+frame = Frame(window)
 frame.pack()
 
 window.mainloop()
